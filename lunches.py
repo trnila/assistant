bf581e45a5cbbe31e998872d80e8ed54e7fd5b3a
Daniel Trnka <daniel.trnka@gmail.com>
2022-03-21 21:11:51 +0100

    lunch: using generators to simplify code

--- lunches.py
+++ lunches.py
@@ -6,6 +6,25 @@ import datetime
 import asyncio
 import itertools
 import traceback
+from dataclasses import dataclass
+
+@dataclass
+class Restaurant:
+    name: str
+    parser: int
+
+@dataclass
+class Soup:
+    name: str
+    price: int = None
+
+@dataclass
+class Lunch:
+    name: str
+    num: int = None
+    price: int = None
+    ingredients: str = None
+
 
 days = ['Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota', 'Neděle']
 
@@ -13,49 +32,27 @@ async def gather_restaurants():
     async with aiohttp.ClientSession() as session:
         async def bistroin():
             async with session.get("https://onemenu.cz/menu/Bistro-In") as r:
-                text = await r.text()
-                dom = BeautifulSoup(text, 'html.parser')
-                foods = []
-                soup = None
-                soup_price = None
+                dom = BeautifulSoup(await r.text(), 'html.parser')
                 for node in dom.select('.orderitem-right'):
                     ingredients = node.select('.ingredients')[0].get_text()
                     ingredients = re.sub('Al\. \(.+', '', ingredients)
                     name = node.select('.name')[0].get_text()
                     price = node.select('.priceValue')[0].get_text().split()[0]
                     if 'Polévka' in name:
-                        soup = name.split(':')[1]
-                        soup_price = price
+                        yield Soup(name=name.split(':')[1], price=price)
                     else:
                         parts = name.split('.', 1)
                         if len(parts) == 2:
-                            foods.append({
-                                'num': parts[0],
-                                'name': parts[1],
-                                'ingredients': ingredients,
-                                'price': price,
-                            })
-                return {
-                    'name': 'Bistro IN',
-                    'soups': [{"name": soup, "price": soup_price}],
-                    'lunches': foods,
-                }
+                            yield Lunch(num=parts[0], name=parts[1], price=price, ingredients=ingredients)
 
         async def u_jarosu():
             async with session.get("https://www.ujarosu.cz/cz/denni-menu/") as r:
-                result = {
-                    'name': 'U Jarošů',
-                    'lunches': [],
-                }
-
-                text = await r.text()
-                dom = BeautifulSoup(text, 'html.parser')
+                dom = BeautifulSoup(await r.text(), 'html.parser')
 
                 day_nth = datetime.datetime.today().weekday()
 
                 counter = 0
                 food = {}
-                foods = []
                 capturing = False
                 for row in dom.findAll('tr'):
                     day = row.select('td')[0].get_text().strip(' \n\t\xa0:')
@@ -64,7 +61,7 @@ async def gather_restaurants():
                             break
                         if day == days[day_nth]:
                             capturing = True
-                            result['soups'] = {"name": row.select('td')[1].get_text()}
+                            yield Soup(name=row.select('td')[1].get_text())
                     elif capturing:
                         spaces = all(not td.get_text().strip() for td in row.select('td'))
                         if spaces:
@@ -77,37 +74,26 @@ async def gather_restaurants():
                         if num == counter + 1:
                             counter += 1
                             if food:
-                                foods.append(food)
-                            food = {
-                                'name': row.select('td')[1].get_text(),
-                                'price': row.select('td')[2].get_text(),
-                                'num': str(num),
-                            }
+                                yield food
+                            food = Lunch(
+                                name=row.select('td')[1].get_text(),
+                                price=row.select('td')[2].get_text(),
+                                num=num,
+                            )
                         else:
-                            food['name'] += ' ' + row.select('td')[1].get_text()
-
+                            food.name += ' ' + row.select('td')[1].get_text()
 
                 if food:
-                    foods.append(food)
-
-                result['lunches'] = foods
-                return result
+                    yield food
 
         async def u_zlateho_lva():
             day_nth = datetime.datetime.today().weekday()
             async with session.get("http://www.zlatylev.com/menu_zlaty_lev.html") as r:
-                text = await r.text()
-                dom = BeautifulSoup(text, 'html.parser')
-                foods = []
+                dom = BeautifulSoup(await r.text(), 'html.parser')
                 text = dom.select('.xr_txt.xr_s0')[0].get_text()
-
-                def remove_alergens(s):
-                    return re.sub('\(A(-|:).*?\)', '', s)
 
                 capturing = False
                 counter = 0
-                food = {}
-                soups = None
                 state = 'name'
                 for line in text.splitlines():
                     line = line.strip()
@@ -117,89 +103,72 @@ async def gather_restaurants():
                     elif capturing:
                         if day_nth < 4 and line.startswith(days[day_nth + 1]):
                             break
-                        print(line, state)
-                        if line.startswith('Polévka:'):
-                            soup = remove_alergens(line)
+                        soup_prefix = 'Polévka:'
+                        if line.startswith(soup_prefix):
+                            yield Soup(line.replace(soup_prefix, ''))
                         else:
                             if state == 'name':
                                 if re.match('^[0-9]+\.', line):
                                     line, name = line.split('.', 1)
-                                    food = {
-                                        'num': line,
-                                        'name': remove_alergens(name),
-                                    }
+                                    food = Lunch(name=name, num=line)
                                     state = 'price'
                             elif state == 'price':
                                 if re.match('^[0-9]+\s*(,-|Kč)$', line):
-                                    food['price'] = line.split(' ')[0]
-                                    foods.append(food)
+                                    food.price = line.split(' ')[0]
+                                    yield food
                                     state = 'name'
-
-                return {
-                    'name': 'U Zlatého Lva',
-                    'soups': {"name": soup} if soup else [],
-                    'lunches': foods,
-                }
 
         async def globus():
             async with session.get("https://www.globus.cz/ostrava/nabidka/restaurace.html") as r:
-                text = await r.text()
-                dom = BeautifulSoup(text, 'html.parser')
-                lunches = []
-                soups = []
+                dom = BeautifulSoup(await r.text(), 'html.parser')
                 for row in dom.select('.restaurant__menu-food-table')[0].select('tr'):
                     tds = row.select('td')
                     name = tds[1].text
                     price = tds[2].text.replace(',–', '')
-
-                    (lunches if int(price) > 50 else soups).append({
-                        'name': name,
-                        'price': price,
-                    })
-
-                return {
-                    'name': 'Globus',
-                    'soups': soups,
-                    'lunches': lunches,
-                }
-
+                    yield (Lunch if int(price) > 50 else Soup)(name=name, price=price)
 
         restaurants = [
-            bistroin,
-            u_jarosu,
-            u_zlateho_lva,
-            globus,
+            Restaurant("Bistro IN", bistroin),
+            Restaurant("U jarosu", u_jarosu),
+            Restaurant("U zlateho lva", u_zlateho_lva),
+            Restaurant("Globus", globus),
         ]
 
-        async def c(f):
+        async def collect(restaurant):
             try:
-                return await f()
+                lunches = []
+                soups = []
+                async for item in restaurant.parser():
+                    if isinstance(item, Soup):
+                        soups.append(item)
+                    elif isinstance(item, Lunch):
+                        lunches.append(item)
+                    else:
+                        raise "Unsupported item"
+                return {
+                    'name': restaurant.name,
+                    'lunches': lunches,
+                    'soups': soups,
+                }
             except:
                 return {"error": traceback.format_exc()}
 
-        foods = await asyncio.gather(*[c(f) for f in restaurants])
-        names = [r.__name__ for r in restaurants]
+        foods = await asyncio.gather(*[collect(r) for r in restaurants])
 
-        def cleanup_foods(foods):
+        def cleanup(restaurant):
             def fix_name(name):
                 uppers = sum(1 for c in name if c.isupper())
                 if uppers > len(name) / 2:
                     name = name.lower()
                     name = name.capitalize()
-                return name
+                name = re.sub('\d+g', '', name)
+                return re.sub('\([^)]+\)', '', name)
 
-            for food in foods.get('lunches', []):
-                for k, v in food.items():
-                    food[k] = v.strip()
-                    if k == 'price':
-                        try:
-                            food[k] = int(food[k])
-                        except ValueError:
-                            pass
-                food['name'] = fix_name(food['name'])
-            if 'soup' in foods:
-                foods['soup'] = fix_name(foods['soup'])
-            return foods
+            for t in ['lunches', 'soups']:
+                for food in restaurant.get(t, []):
+                    if food.price:
+                        food.price = int(food.price.replace(',-', ''))
+                    food.name = fix_name(food.name)
+            return restaurant
 
-
-        return map(cleanup_foods, foods)
+        return map(cleanup, foods)
