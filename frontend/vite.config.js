7a7380db21c5e09f542403258da0adf9804ffb50
Daniel Trnka <daniel.trnka@gmail.com>
2022-10-28 18:12:53 +0200

    changed API port for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/lunch.json': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5001',
       }
     }
   }
