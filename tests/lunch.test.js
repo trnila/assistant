c7b591dab52aa6756173def6ce0f34f05b19b548
Daniel Trnka <daniel.trnka@gmail.com>
2025-09-08 00:40:06 +0200

    tests: increased timeout to 120secs as some pages may fail several times

--- tests/lunch.test.js
+++ tests/lunch.test.js
@@ -1,5 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
+test.setTimeout(120_000)
+
 test('smoke test', async ({ page }) => {
   await page.goto('http://localhost:8000');
   const content = await page.textContent('.restaurant h2:first-of-type');
